0af04d1d54711bedb609e04afb2c28375a93645f
Hryhorii Victorovich Ilin <devopsick@gmail.com>
2026-03-15 19:12:51 +0000

    report

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+  adapter: cloudflare(),
+});
